6c3e2ee5ed04c08807af2dad430ee629f3b59990
Eren ERDOĞAN <imerenerdogan@gmail.com>
2020-09-22 21:29:16 +0300

    Site Complete
    I have completed the site

---
+++ index.js
@@ -0,0 +1,3 @@
+let forYear = new Date().getFullYear() - 2013;
+document.getElementById("skillIntro").innerHTML = "Hey! You can see my skillset here, I am constantly learning new things and I make small websites and apps for " + forYear + " years!";
+document.getElementById("footerText").innerHTML = "Eren ERDOGAN &copy" + new Date().getFullYear();
